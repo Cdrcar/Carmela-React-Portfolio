cf3c00b8cc01824eb6ed75e53efc12e45dfaf89e
Carmela <carmela881@outlook.com>
2023-07-20 08:25:14 +0100

    Updates

---
+++ precache-manifest.ac727e2f0736cb115ba2c67f1d125144.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4ac4f911c26ddb2ad2e",
+    "url": "/Carmela-React-Portfolio/static/css/main.f8ba631e.chunk.css"
+  },
+  {
+    "revision": "d4ac4f911c26ddb2ad2e",
+    "url": "/Carmela-React-Portfolio/static/js/main.0dbedb4c.chunk.js"
+  },
+  {
+    "revision": "6591eaa4d2728b9da474",
+    "url": "/Carmela-React-Portfolio/static/js/runtime~main.570115d6.js"
+  },
+  {
+    "revision": "f32c9fc4ab1053c8c371",
+    "url": "/Carmela-React-Portfolio/static/css/2.23a24373.chunk.css"
+  },
+  {
+    "revision": "f32c9fc4ab1053c8c371",
+    "url": "/Carmela-React-Portfolio/static/js/2.0217767c.chunk.js"
+  },
+  {
+    "revision": "b980f434cf516334bcff38e21d3d042a",
+    "url": "/Carmela-React-Portfolio/static/media/cv.b980f434.pdf"
+  },
+  {
+    "revision": "7e4969a70f9bf459ab43ed96eabe6417",
+    "url": "/Carmela-React-Portfolio/static/media/car-logo.7e4969a7.png"
+  },
+  {
+    "revision": "fafa30477192e97505858bb7e1dafebd",
+    "url": "/Carmela-React-Portfolio/static/media/my-photo.fafa3047.jpg"
+  },
+  {
+    "revision": "e399e437cb30936308f8960ab27b461f",
+    "url": "/Carmela-React-Portfolio/static/media/sociosynergy.e399e437.png"
+  },
+  {
+    "revision": "339a687a304179456476aca0a238e80d",
+    "url": "/Carmela-React-Portfolio/static/media/technovate.339a687a.png"
+  },
+  {
+    "revision": "e5e3d73c943ba9868c8f50cfa20bb0dd",
+    "url": "/Carmela-React-Portfolio/static/media/weather.e5e3d73c.png"
+  },
+  {
+    "revision": "e0e91bf8d309e366d6091339831547d0",
+    "url": "/Carmela-React-Portfolio/static/media/quiz.e0e91bf8.png"
+  },
+  {
+    "revision": "cd2dbd0dc8ad35025bdabe66ca4c98bc",
+    "url": "/Carmela-React-Portfolio/static/media/password.cd2dbd0d.png"
+  },
+  {
+    "revision": "ff9c9786db52a7f59d71854821a62ed6",
+    "url": "/Carmela-React-Portfolio/static/media/jate.ff9c9786.png"
+  },
+  {
+    "revision": "86fc503619558da13cb6d96b653522ff",
+    "url": "/Carmela-React-Portfolio/static/media/wizdumb.86fc5036.png"
+  },
+  {
+    "revision": "2ecba96859a2aa673db57ec04e57c9c1",
+    "url": "/Carmela-React-Portfolio/index.html"
+  }
+];
